cad660c67013262ba3a31cbcf6e238fc479a681a
tahirdibirov <tahirdibirovsecond@gmail.com>
2025-10-23 15:17:34 +0400

    chore: ensure hooks run tests and lint-staged

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
+
+// Allow vitest globals in test files
+module.exports.overrides = [
+  {
+    files: ['test/**/*.ts', 'test/**/*.test.ts'],
+    env: {
+      browser: true,
+      node: true,
+    },
+    globals: {
+      describe: 'readonly',
+      it: 'readonly',
+      expect: 'readonly',
+      beforeEach: 'readonly',
+      afterEach: 'readonly',
+    },
+  },
+];
